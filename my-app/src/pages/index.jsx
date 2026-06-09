89a17d8a3a5e5030a804f8b4b940bf1ea906f9c0
Jonah, KemfonAbasi Mfon <kemfonabasijonah@gmail.com>
2026-06-09 09:58:44 +0100

    fix: add vercel SPA rewrite config

--- my-app/src/pages/index.jsx
+++ my-app/src/pages/index.jsx
@@ -3,14 +3,15 @@ import bgImage from '../assets/LandingPage_Image.png'
 function Hero() {
   return (
     <section
-      className="w-full py-40 flex justify-center items-center bg-cover bg-center bg-no-repeat"
-      style={{ backgroundImage: `url(${bgImage})` }}
+      className="w-full py-50 flex justify-center items-center bg-cover bg-right bg-no-repeat"
+      // style={{ backgroundImage: `url(${bgImage})` }}
+      style={{ backgroundImage: `linear-gradient(to bottom, rgba(0,0,0,0.3), rgba(0,0,0,0.3)), url(${bgImage})` }} 
     >
       <div className="w-[90%] flex justify-end">
-        <div className="w-full bg-amber-950 text-center font-[serif] text-[white]">
-          <h1 className="text-[3rem] leading-16 tracking-[2px] font-medium">Glow with Aloe Naturally. Radiate Confidence.</h1>
-          <p className="leading-7 tracking-[1px] max-w-153.75 text-[1.4rem] self-end bg-red">Experience the purity of nature blended with science-backed skincare made to nurture, not overwhelm.</p>
-          <button className="button" type="button">Shop Now</button>
+        <div className="w-full text-center font-[serif] text-[white]">
+          <h1 className="text-[3.1rem] leading-[65px] tracking-[1px] font-medium">Glow with Aloe Naturally. Radiate Confidence.</h1>
+          <p className="leading-7.5 tracking-[1px] text-[1.35rem] self-end mt-3 mb-8">Experience the purity of nature blended with science-backed skincare made to nurture, not overwhelm.</p>
+          <button className="w-full py-3 bg-[white] text-[black] text-[1.4rem] rounded-full font-[500]" type="button">Shop Now</button>
         </div>
       </div>
     </section>
