90180d546dd1cbb05a72ba7c0336677cedecdbf1
Jonah, KemfonAbasi Mfon <kemfonabasijonah@gmail.com>
2026-06-09 12:53:05 +0100

    your message

--- my-app/src/pages/index.jsx
+++ my-app/src/pages/index.jsx
@@ -6,7 +6,7 @@ import imgfour from '../assets/ProductBottled_Image.png'
 
 function Card(props) {
   return (
-    <div className="flex flex-col gap-2">
+    <div className="flex flex-col gap-3">
       <img src={img} alt="" className="block rounded-lg w-full" />
       <div className="flex items-center flex-col">
         <p className="text-[16px] sm:text-[12px] md:text-[13px] font-sans">Lovry Aloe Serum</p>
@@ -24,7 +24,7 @@ function Hero() {
     <>
       {/* Hero / Landing Section */}
       <section
-        className="w-full py-32 sm:py-48 md:py-60 lg:py-70 flex justify-center items-center bg-cover bg-center bg-no-repeat"
+        className="w-full py-45 sm:py-48 md:py-60 lg:py-70 flex justify-center items-center bg-cover bg-center bg-no-repeat"
         style={{ backgroundImage: `linear-gradient(to bottom, rgba(0,0,0,0.2), rgba(0,0,0,0.2)), url(${bgImage})` }}
       >
         <div className="w-[90%]">
@@ -66,7 +66,7 @@ function Hero() {
         <div className="w-[90%] flex flex-col justify-center items-center gap-1 text-white text-center">
           <h1 className="font-[serif] text-[26px] sm:text-[32px] md:text-[39px]">Nature's Heroes, Bottled</h1>
           <p className="text-[17px] sm:text-[18px] md:text-[23px] leading-[1.5] font-[serif] max-w-xl md:max-w-none">
-            Discover our latest skincare must-haves—thoughtfully crafted<br className="hidden md:block" /> with skin-loving botanicals and luxurious textures.
+            Discover our latest skincare must-haves — thoughtfully crafted<br className="hidden md:block" /> with skin-loving botanicals and luxurious textures.
           </p>
           <button
             type="button"
@@ -120,53 +120,53 @@ function Hero() {
         <div className="w-[90%] flex flex-col gap-8">
           <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-8 sm:gap-6">
             <div className="flex flex-col gap-3">
-              <h2 className="text-[1rem] font-bold text-black">About Us</h2>
-              <a href="" className="text-[14px] sm:text-[15px]">Mission Statement</a>
-              <a href="" className="text-[14px] sm:text-[15px]">Blog</a>
-              <a href="" className="text-[14px] sm:text-[15px]">Careers</a>
-              <a href="" className="text-[14px] sm:text-[15px]">Press Centre</a>
+              <h2 className="text-[1.1rem] font-bold text-black">About Us</h2>
+              <a href="" className="text-[1.07rem] sm:text-[15px]">Mission Statement</a>
+              <a href="" className="text-[1.07rem] sm:text-[15px]">Blog</a>
+              <a href="" className="text-[1.07rem] sm:text-[15px]">Careers</a>
+              <a href="" className="text-[1.07rem] sm:text-[15px]">Press Centre</a>
             </div>
 
             <div className="flex flex-col gap-3">
-              <h2 className="text-[1rem] font-bold text-black">Support</h2>
-              <a href="" className="text-[14px] sm:text-[15px]">Product Guide</a>
-              <a href="" className="text-[14px] sm:text-[15px]">Live Chat/Help Desk</a>
-              <a href="" className="text-[14px] sm:text-[15px]">Product Update</a>
-              <a href="" className="text-[14px] sm:text-[15px]">Community Forum</a>
+              <h2 className="text-[1.1rem] font-bold text-black">Support</h2>
+              <a href="" className="text-[1.07rem] sm:text-[15px]">Product Guide</a>
+              <a href="" className="text-[1.07rem] sm:text-[15px]">Live Chat/Help Desk</a>
+              <a href="" className="text-[1.07rem] sm:text-[15px]">Product Update</a>
+              <a href="" className="text-[1.07rem] sm:text-[15px]">Community Forum</a>
             </div>
 
             <div className="flex flex-col gap-3">
-              <h2 className="text-[1rem] font-bold text-black">FAQ</h2>
-              <a href="" className="text-[14px] sm:text-[15px]">How to know my skin type</a>
-              <a href="" className="text-[14px] sm:text-[15px]">Shipping and Delivery</a>
-              <a href="" className="text-[14px] sm:text-[15px]">Return Policy</a>
-              <a href="" className="text-[14px] sm:text-[15px]">Account Management</a>
+              <h2 className="text-[1.1rem] font-bold text-black">FAQ</h2>
+              <a href="" className="text-[1.07rem] sm:text-[15px]">How to know my skin type</a>
+              <a href="" className="text-[1.07rem] sm:text-[15px]">Shipping and Delivery</a>
+              <a href="" className="text-[1.07rem] sm:text-[15px]">Return Policy</a>
+              <a href="" className="text-[1.07rem] sm:text-[15px]">Account Management</a>
             </div>
 
             <div className="flex flex-col gap-3">
-              <h2 className="text-[1rem] font-bold text-black">Product</h2>
-              <a href="" className="text-[14px] sm:text-[15px]">Features</a>
-              <a href="" className="text-[14px] sm:text-[15px]">Size Guide</a>
-              <a href="" className="text-[14px] sm:text-[15px]">Sustainability</a>
-              <a href="" className="text-[14px] sm:text-[15px]">Return Policy</a>
+              <h2 className="text-[1.1rem] font-bold text-black">Product</h2>
+              <a href="" className="text-[1.07rem] sm:text-[15px]">Features</a>
+              <a href="" className="text-[1.07rem] sm:text-[15px]">Size Guide</a>
+              <a href="" className="text-[1.07rem] sm:text-[15px]">Sustainability</a>
+              <a href="" className="text-[1.07rem] sm:text-[15px]">Return Policy</a>
             </div>
           </div>
 
           <div className="flex flex-col sm:flex-row justify-between items-start sm:items-center gap-6 sm:gap-0">
-            <div className="flex flex-col gap-3">
-              <h2 className="text-[0.85rem] sm:text-[0.9rem] text-black font-semibold">Sign Up for Email Deals & Cashback</h2>
+            <div className="flex flex-col gap-4">
+              <h2 className="text-[1rem] sm:text-[0.9rem] text-black font-semibold">Sign Up for Email Deals & Cashback</h2>
               <div className="flex overflow-hidden rounded-lg bg-amber-50">
                 <input
                   type="text"
-                  className="px-4 outline-0 text-[13px] sm:text-[14px]"
+                  className="px-4 outline-0 text-[16px] sm:text-[14px]"
                   placeholder="Enter email address"
                 />
-                <button type="button" className="py-2.5 px-5 sm:px-6 text-[13px] sm:text-[14px] text-white bg-[#547c24]">
+                <button type="button" className="py-2.5 px-5 sm:px-6 text-[16px] sm:text-[14px] text-white bg-[#547c24]">
                   Submit
                 </button>
               </div>
             </div>
-            <div className="flex gap-4 text-[1.3rem] sm:text-[1.5rem]">
+            <div className="flex gap-2 text-[1.4rem] sm:text-[.7rem] justify-center self-center">
               <i className="fa-brands fa-x-twitter"></i>
               <i className="fa-brands fa-tiktok"></i>
               <i className="fa-brands fa-square-facebook"></i>
